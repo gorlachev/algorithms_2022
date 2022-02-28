50fb76a3fcadff6dec7df5731d62687b0056a41f
Sergey Gorlachev <sgorlachev@gmail.com>
2022-02-28 22:30:07 +0300

    Algorithms - Lesson 6

--- Урок 6. Практическое задание/task_1_4.py
+++ Урок 6. Практическое задание/task_1_4.py
@@ -30,3 +30,127 @@
 
 Это файл для четвертого скрипта
 """
+
+
+# Урок 9 - Задание 4. Реализуйте базовый класс Car:
+# у класса должны быть следующие атрибуты: speed, color, name, is_police (булево). А также методы:
+# go, stop, turn(direction), которые должны сообщать, что машина поехала, остановилась, повернула (куда);
+# опишите несколько дочерних классов: TownCar, SportCar, WorkCar, PoliceCar;
+# добавьте в базовый класс метод show_speed, который должен показывать текущую скорость автомобиля;
+# для классов TownCar и WorkCar переопределите метод show_speed. При значении скорости свыше 60 (TownCar)
+# и 40 (WorkCar) должно выводиться сообщение о превышении скорости.
+
+from pympler import asizeof
+
+
+class Car1:
+    def __init__(self, speed, color, name, is_police):
+        self.speed = speed
+        self.color = color
+        self.name = name
+        self.is_police = is_police
+        self.direction = "прямо"
+
+    def go(self):
+        print("Машина поехала.")
+
+    def stop(self):
+        print("Машина остановилась.")
+
+    def turn(self, direction):
+        self.direction = direction
+        print(f"Машина едет {self.direction}.")
+
+    def show_speed(self):
+        print(f"Текущая скорость: {self.speed} км/ч.")
+
+
+class TownCar1(Car1):
+    def __init__(self, speed, color, name, is_police=False):
+        super().__init__(speed, color, name, is_police)
+
+    def show_speed(self):
+        print(f"Текущая скорость: {self.speed} км/ч.")
+        if self.speed > 60:
+            print("Вы превышаете допустимую скорость")
+
+
+class SportCar1(Car1):
+    def __init__(self, speed, color, name, is_police=False):
+        super().__init__(speed, color, name, is_police)
+
+
+class WorkCar1(Car1):
+    def __init__(self, speed, color, name, is_police=False):
+        super().__init__(speed, color, name, is_police)
+
+    def show_speed(self):
+        print(f"Текущая скорость: {self.speed} км/ч.")
+        if self.speed > 40:
+            print("Вы превышаете допустимую скорость")
+
+
+class PoliceCar1(Car1):
+    def __init__(self, speed, color, name, is_police=True):
+        super().__init__(speed, color, name, is_police)
+
+# оптимизация памяти через слоты
+
+class Car2:
+    __slots__ = ("speed", "color", "name", "is_police")
+
+    def __init__(self, speed, color, name, is_police):
+        self.speed = speed
+        self.color = color
+        self.name = name
+        self.is_police = is_police
+        self.direction = "прямо"
+
+    def go(self):
+        print("Машина поехала.")
+
+    def stop(self):
+        print("Машина остановилась.")
+
+    def turn(self, direction):
+        self.direction = direction
+        print(f"Машина едет {self.direction}.")
+
+    def show_speed(self):
+        print(f"Текущая скорость: {self.speed} км/ч.")
+
+
+class TownCar2(Car2):
+    def __init__(self, speed, color, name, is_police=False):
+        super().__init__(speed, color, name, is_police)
+
+    def show_speed(self):
+        print(f"Текущая скорость: {self.speed} км/ч.")
+        if self.speed > 60:
+            print("Вы превышаете допустимую скорость")
+
+
+class SportCar2(Car2):
+    def __init__(self, speed, color, name, is_police=False):
+        super().__init__(speed, color, name, is_police)
+
+
+class WorkCar2(Car2):
+    def __init__(self, speed, color, name, is_police=False):
+        super().__init__(speed, color, name, is_police)
+
+    def show_speed(self):
+        print(f"Текущая скорость: {self.speed} км/ч.")
+        if self.speed > 40:
+            print("Вы превышаете допустимую скорость")
+
+
+class PoliceCar2(Car2):
+    def __init__(self, speed, color, name, is_police=True):
+        super().__init__(speed, color, name, is_police)
+
+
+print(asizeof.asizeof(PoliceCar1(60, "yellow", "Lexus")))  # 712
+print(asizeof.asizeof(PoliceCar2(60, "yellow", "Lexus")))  # 512
+
+
--- Урок 6. Практическое задание/task_1_5.py
+++ Урок 6. Практическое задание/task_1_5.py
@@ -30,3 +30,51 @@
 
 Это файл для пятого скрипта
 """
+
+# Урок 5 - Задание 5. Представлен список чисел. Определить элементы списка, не имеющие повторений.
+# Сформировать из этих элементов список с сохранением порядка их следования в исходном списке, например:
+# src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
+# result = [23, 1, 3, 10, 4, 11]
+# Подсказка: напишите сначала решение «в лоб». Потом подумайте об оптимизации.
+
+from memory_profiler import memory_usage
+
+
+def decor(func):
+    def wrapper(*args, **kwargs):
+        m1 = memory_usage()
+        res = func(args[0])
+        m2 = memory_usage()
+        mem_diff = m2[0] - m1[0]
+        return res, mem_diff
+    return wrapper
+
+
+@decor
+def count1(src):
+    return [i for i in src if src.count(i) == 1]
+
+# оптимизация памяти через использование генератора
+
+
+@decor
+def count2(src):
+    return (i for i in src if src.count(i) == 1)
+
+
+res, mem_diff = count1(list(range(10000)))
+print(f"Выполнение заняло {mem_diff} Mib")  # Выполнение заняло 0.12890625 Mib
+
+my_generator, mem_diff = count2(list(range(10000)))
+print(type(my_generator))
+for i in my_generator:
+    print(i)
+print(f"Выполнение заняло {mem_diff} Mib")  # Выполнение заняло 0.00390625 Mib
+
+
+
+
+
+
+
+
